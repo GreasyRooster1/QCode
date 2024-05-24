951adb847e65037ae69623a3434196010bb32e5f
QuantumDeity <greasyRooster1@gmail.com>
2024-05-23 21:12:15 -0700

    added step element

--- global/customElements.js
+++ global/customElements.js
@@ -11,4 +11,36 @@ class GutterElement extends HTMLElement {
     }
 }
 
-customElements.define("gutter", GutterElement);
+class StepElement extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes() {
+        return ['type'];
+    }
+
+    connectedCallback() {
+        const head = document.createElement("div");
+        head.classList.add("step-head");
+
+
+        const content = document.createElement("div");
+        content.classList.add("step-content");
+
+        this.appendChild(head);
+        this.appendChild(content);
+
+        this.classList.add("step");
+        this.style.display = "block";
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if(oldValue !== newValue) {
+            this.classList.add(newValue);
+        }
+    }
+}
+
+customElements.define("editor-gutter", GutterElement);
+customElements.define("editor-step", StepElement);
