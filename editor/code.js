35141fc89e6d1388ef32ab0ce002af7b64dcab93
greasyRooster1 <GreasyRooster1@gmail.com>
2024-05-24 09:58:44 -0700

    fix hold enter

--- editor/code.js
+++ editor/code.js
@@ -1,9 +1,8 @@
 const textarea = document.querySelector(".code-editor > .text-input");
 const numbers = document.querySelector(".code-editor > .numbers");
-textarea.addEventListener("keyup", (e) => {
-    const num = e.target.value.split("\n").length;
+textarea.addEventListener("keydown", (e) => {
+    const num = e.target.value.split("\n").length+1;
     numbers.innerHTML = Array(num).fill("<span></span>").join("");
-
 });
 textarea.addEventListener("keydown", (event) => {
     if (event.key === "Tab") {
