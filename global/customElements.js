5cf47ac8778b819c8fa4ad8d8f9ab35c1b5c639a
QuantumDeity <greasyRooster1@gmail.com>
2024-05-23 20:57:16 -0700

    rename handle to gutter

--- global/customElements.js
+++ global/customElements.js
@@ -1,14 +1,14 @@
-class EditorHandleElement extends HTMLElement {
+class GutterElement extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
         const block = document.createElement("div");
-        block.classList.add("edit-handle-block");
+        block.classList.add("gutter-block");
         this.appendChild(block);
         this.classList.add("gutter");
     }
 }
 
-customElements.define("edit-handle", EditorHandleElement);
+customElements.define("gutter", GutterElement);
