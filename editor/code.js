e3922c9cc2ffe4008dfc513b50d1bd6120c5b717
greasyRooster1 <GreasyRooster1@gmail.com>
2024-05-24 10:52:12 -0700

    this is dumb

--- editor/code.js
+++ editor/code.js
@@ -1,19 +1,5 @@
-const textarea = document.querySelector(".code-editor > .text-input");
-const numbers = document.querySelector(".code-editor > .numbers");
-textarea.addEventListener("keydown", (e) => {
-    const num = e.target.value.split("\n").length+1;
-    numbers.innerHTML = Array(num).fill("<span></span>").join("");
-});
-textarea.addEventListener("keydown", (event) => {
-    if (event.key === "Tab") {
-        const start = textarea.selectionStart;
-        const end = textarea.selectionEnd;
+const codeInput = document.querySelector(".code-input");
 
-        textarea.value =
-            textarea.value.substring(0, start) +
-            "\t" +
-            textarea.value.substring(end);
-
-        event.preventDefault();
-    }
+codeInput.addEventListener("input", (e) => {
+    e.target.querySelector(":scope > br").remove()
 });
