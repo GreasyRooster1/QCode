3c99583b10aea62c7c905b211ed19e30dad4ba26
QuantumDeity <greasyRooster1@gmail.com>
2024-05-27 15:40:57 -0700

    added split.js support

---
+++ editor/js/panes.js
@@ -0,0 +1,9 @@
+const stepsPane = document.querySelector(".steps-pane");
+const codePane = document.querySelector(".code-pane");
+const outputPane = document.querySelector(".output-pane");
+const leftGutter = document.querySelector(".gutter-left");
+const rightGutter = document.querySelector(".gutter-right");
+const wrapper = document.querySelector(".pane-container");
+
+
+Split(['.steps-pane', '.code-pane', '.output-pane']);
