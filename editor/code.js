2aec004e3a4a60170ebea481a6c31ed46becb342
QuantumDeity <greasyRooster1@gmail.com>
2024-05-27 14:36:43 -0700

    added linting

--- editor/code.js
+++ editor/code.js
@@ -1,10 +1,12 @@
-CodeMirror(document.querySelector('.code-editor'), {
-    lineNumbers: true,
-    tabSize: 2,
-    value: 'console.log("Hello, World");',
-    mode: 'javascript',
-    theme: 'default',
-    state: EditorState.create({
-        extensions: [json(), linterExtension]
-    }),
-});
+import { basicSetup, EditorView} from "codemirror/codemirror/dist/index.js"
+import { javascript } from "codemirror/lang-javascript/dist/index.js"
+import { linter } from 'codemirror/lint/dist/index.js'
+import { javascriptLanguage } from 'codemirror/lang-javascript/dist/index.js'
+
+const linterExtension = linter(javascriptLanguage);
+
+new EditorView({
+    doc: "console.log('hello')\n",
+    extensions: [basicSetup, javascript(), linterExtension],
+    parent: document.querySelector(".code-editor"),
+})
