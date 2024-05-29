2e363f6f078d5cf5d9b92b5d0b7b5bbdedb6405a
greasyRooster1 <GreasyRooster1@gmail.com>
2024-05-29 09:23:38 -0700

    show auth error

--- login/js/login.js
+++ login/js/login.js
@@ -2,6 +2,9 @@ const loginButton = document.querySelector(".login-button");
 const emailInput = document.querySelector(".email-input");
 const passwordInput = document.querySelector(".password-input");
 
+const authErrorDisplayWrapper = document.querySelector(".auth-error");
+const authErrorContent = document.querySelector(".auth-error-message");
+
 loginButton.addEventListener("click", function(){
     let email = emailInput.value;
     let password = passwordInput.value;
@@ -12,12 +15,16 @@ loginButton.addEventListener("click", function(){
             currentUser =  userCredential.user;
         })
         .catch((error) => {
-            let errorType = handleAuthErrors(error.code,error.message);
+            let errorType = handleAuthErrors(error);
             switch (errorType){
-                case "invalid credentials": break;
-                case "invalid email": break;
+                case "invalid credentials": showAuthError("Email or password is incorrect"); break;
+                case "invalid email": showAuthError("Incorrect email format");break;
                 case "ok": break;
             }
         });
-
 });
+
+function showAuthError(msg){
+    authErrorDisplayWrapper.style.visibility = "true"
+    authErrorContent.innerHTML = msg;
+}
