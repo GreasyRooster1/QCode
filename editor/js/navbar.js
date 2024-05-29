1bd515c8ae4000e622cc753febf78e7bca396f0e
QuantumDeity <greasyRooster1@gmail.com>
2024-05-29 16:42:25 -0700

    added grow and shrink

--- editor/js/navbar.js
+++ editor/js/navbar.js
@@ -1,6 +1,10 @@
 const navbar = document.querySelector(".navbar");
 const navbarShrinkButton = document.querySelector(".navbar-shrink-button");
+const navbarGrowButton = document.querySelector(".navbar-grow-button");
 
 navbarShrinkButton.addEventListener("click",function (){
     navbar.style.height = "0";
+});
+navbarGrowButton.addEventListener("click",function (){
+    navbar.style.height = "5vh";
 });
