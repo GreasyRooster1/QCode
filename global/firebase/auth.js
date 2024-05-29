d8ab8b7a6949014ae4fa5c200974ac5c461614bb
greasyRooster1 <GreasyRooster1@gmail.com>
2024-05-29 08:57:12 -0700

    update auth.js

--- global/firebase/auth.js
+++ global/firebase/auth.js
@@ -1,15 +1,41 @@
 //https://stackoverflow.com/questions/59935608/solved-get-user-auth-data-from-firebase-add-it-to-firebase-db
 
+let currentUser = null;
+
 function createUser(email,password){
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            // Signed in
-            var user = userCredential.user;
-            console.log(user);
+            let user = userCredential.user;
+            console.log("created user");
+            currentUser = user;
         })
         .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode,errorMessage);
+            handleAuthErrors(error.code,error.message)
         });
+}
+
+function logInUser(email,password){
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            let user = userCredential.user;
+            console.log("logged in user");
+            currentUser = user;
+        })
+        .catch((error) => {
+            handleAuthErrors(error.code,error.message)
+        });
+
+}
+
+function logOutUser(){
+    firebase.auth().signOut().then(() => {
+        console.log("logged out user");
+        currentUser = null;
+    }).catch((error) => {
+        handleAuthErrors(error.code,error.message);
+    });
+}
+
+function handleAuthErrors(errorCode,errorMessage){
+    console.error("Auth error: ",errorCode,errorMessage);
 }
