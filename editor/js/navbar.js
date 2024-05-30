824fc0f18ba4aa83a7e6ddf1ba623e04f55b6302
QuantumDeity <greasyRooster1@gmail.com>
2024-05-29 21:34:37 -0700

    styled navbar

--- editor/js/navbar.js
+++ editor/js/navbar.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector(".navbar");
 const navbarVisibilityButton = document.querySelector(".navbar-visibility-button");
 const userLink = document.querySelector(".username-link");
 const accountOptions = document.querySelector(".account-options");
+const accountOptionsWrapper = document.querySelector(".account-dropdown-wrapper");
 const logoutButton = document.querySelector(".logout-button");
 
 let navbarVisible = true;
@@ -15,10 +16,16 @@ navbarVisibilityButton.addEventListener("click",function (){
     navbarVisible = !navbarVisible;
     if(navbarVisible){
         navbar.style.height = "5vh";
+        setTimeout(function () {
+            accountOptionsWrapper.style.display = "block";
+        },400);
         navbarVisibilityButton.style.transform = "scaleY(1) translateY(0px)";
     }else{
         navbar.style.height = "0";
         navbarVisibilityButton.style.transform = "scaleY(-1) translateY(5px)";
+        setTimeout(function () {
+            accountOptionsWrapper.style.display = "none";
+        },400);
     }
 });
 
