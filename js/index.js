41997066cd9fd8bd6e718cbfba00ebd1f38a56b9
greasyRooster1 <GreasyRooster1@gmail.com>
2024-05-31 11:03:22 -0700

    broke everything

--- js/index.js
+++ js/index.js
@@ -14,13 +14,22 @@ function loadBadges(){
             let badgeConnect = firebase.database().ref().child("badges").child(badge.id).get();
             badgeConnect.then((snap)=>{
                 let badgeProperties = snap.val();
+
                 let badgeElement = document.createElement("div");
                 badgeElement.classList.add("badge");
+
                 let img = document.createElement("img");
                 img.setAttribute("src",badgeProperties.image);
                 img.setAttribute("alt",badgeProperties.name+" badge");
                 img.setAttribute("title",badgeProperties.name);
+
+                let hoverText = document.createElement("div");
+                hoverText.classList.add("badge-desc");
+                hoverText.innerHTML = badgeProperties.description;
+
                 badgeElement.appendChild(img);
+                badgeElement.appendChild(hoverText);
+
                 badgeDisplay.appendChild(badgeElement)
             });
         }
