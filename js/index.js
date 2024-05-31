06e578bdc84688cefaef1f9ba581b5e8d1cdb496
greasyRooster1 <GreasyRooster1@gmail.com>
2024-05-31 10:39:42 -0700

    loading badges

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const badgeDisplay = document.querySelector(".badges-display");
+
+
+function loadBadges(){
+    let user = getStoredUser();
+    let badgesRef = database.ref('userdata/'+user.uid+"/badges");
+    badgesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+
+        badgeDisplay.innerHTML = "";
+        for(let badge of data){
+
+
+            let badgeConnect = firebase.database().ref().child("badges").child(badge.id).get();
+            badgeConnect.then((snap)=>{
+                let badgeProperties = snap.val();
+                let badgeElement = document.createElement("div");
+                badgeElement.classList.add("badge");
+                let img = document.createElement("img");
+                img.setAttribute("src",badgeProperties.image);
+                img.setAttribute("alt",badgeProperties.name);
+                badgeElement.appendChild(img);
+                badgeDisplay.appendChild(badgeElement)
+            });
+        }
+    });
+}
+
+loadBadges();
