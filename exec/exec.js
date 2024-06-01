98d3f41a10d9f6f659a87edc4dbf751c29966243
QuantumDeity <greasyRooster1@gmail.com>
2024-05-31 21:06:11 -0700

    added test

--- exec/exec.js
+++ exec/exec.js
@@ -1,23 +1,39 @@
-
+const test = 1;
 
 function runJs(js){
-    //clear dangerous objects
+    //clear dangerous objects and run code
     eval(`
         window=null;
         document=null;
         XMLHttpRequest=null;
         XMLHttpRequestUpload=null;
-        eval = null;
-    `);
+        setup=null;
+    `+js);
+}
 
-    //run the js
-    eval(js);
+function canvasTest(){
+    background(0);
+    stroke(255);
+
+    fill(0,0,0);
+    rect(0,0,50,50);
+
+    fill(255,0,0);
+    rect(450,0,50,50);
+
+    fill(255,255,0);
+    rect(450,450,50,50);
+
+    fill(0,255,0);
+    rect(0,450,50,50);
+
+    fill(255);
+    textAlign(CENTER);
+    text("Welcome back",250,250);
 }
 
 function setup(){
     createCanvas(500,500);
-    rect(0,0,100,100);
-    rect(400,400,100,100);
 
     document.getElementById("defaultCanvas0").style.width = "100vmin";
     document.getElementById("defaultCanvas0").style.height = "100vmin";
