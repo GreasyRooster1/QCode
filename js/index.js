28016d17228063682d9dec5a59221d4cff5fba9d
QuantumDeity <greasyRooster1@gmail.com>
2024-06-02 00:02:45 -0700

    creating user

--- js/index.js
+++ js/index.js
@@ -1,44 +1,60 @@
 const badgeDisplay = document.querySelector(".badges-display");
+let user = getStoredUser();
 
+function init(){
+    checkUserDataExists();
+    loadBadges();
+}
+
+function checkUserDataExists(){
+    let userDataFolder = database.ref('userdata/');
+
+    try{
+        userDataFolder.get(user.uid);
+    }catch(e){
+        console.log("user does not exist!")
+    }
+}
 
 function loadBadges(){
-    let user = getStoredUser();
     let badgesRef = database.ref('userdata/'+user.uid+"/badges");
     badgesRef.on('value', (snapshot) => {
         const data = snapshot.val();
 
         badgeDisplay.innerHTML = "";
         for(let badge of data){
-
-
             let badgeConnect = firebase.database().ref().child("badges").child(badge.id).get();
             badgeConnect.then((snap)=>{
-                let badgeProperties = snap.val();
-
-                let badgeElement = document.createElement("div");
-                badgeElement.classList.add("badge");
-
-                let img = document.createElement("img");
-                img.setAttribute("src",badgeProperties.image);
-                img.setAttribute("alt",badgeProperties.name+" badge");
-                img.setAttribute("title",badgeProperties.name);
-
-                let hoverText = document.createElement("div");
-                hoverText.classList.add("badge-desc");
-
-                let hoverTextContent = document.createElement("div");
-                hoverTextContent.classList.add("badge-desc-text");
-                hoverTextContent.innerHTML = badgeProperties.value+"pts";
-
-                hoverText.appendChild(hoverTextContent);
-
-                badgeElement.appendChild(img);
-                badgeElement.appendChild(hoverText);
-
-                badgeDisplay.appendChild(badgeElement)
+                createBadgeElementFromSnap(snap)
             });
         }
     });
 }
 
-loadBadges();
+function createBadgeElementFromSnap(snap){
+    let badgeProperties = snap.val();
+
+    let badgeElement = document.createElement("div");
+    badgeElement.classList.add("badge");
+
+    let img = document.createElement("img");
+    img.setAttribute("src",badgeProperties.image);
+    img.setAttribute("alt",badgeProperties.name+" badge");
+    img.setAttribute("title",badgeProperties.name);
+
+    let hoverText = document.createElement("div");
+    hoverText.classList.add("badge-desc");
+
+    let hoverTextContent = document.createElement("div");
+    hoverTextContent.classList.add("badge-desc-text");
+    hoverTextContent.innerHTML = badgeProperties.value+"pts";
+
+    hoverText.appendChild(hoverTextContent);
+
+    badgeElement.appendChild(img);
+    badgeElement.appendChild(hoverText);
+
+    badgeDisplay.appendChild(badgeElement)
+}
+
+init();
