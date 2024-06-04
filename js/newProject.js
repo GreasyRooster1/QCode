84a6b076f52a8cd894ddbca49b14c40df1e9cef6
QuantumDeity <greasyRooster1@gmail.com>
2024-06-03 19:35:52 -0700

    prompt when making new project

--- js/newProject.js
+++ js/newProject.js
@@ -1,13 +1,13 @@
 const newProjectButton = document.querySelector('.new-project-button');
 
 newProjectButton.addEventListener('click', (e) => {
-    //createProject();
+    createProject(prompt("name for new project"));
 })
 
 function createProject(projectName){
     let user = getStoredUser();
-    let date = new Date();
-    database.ref("userdata/"+user.uid+"/projects").child(projectName).set({
+    let projectId = projectName.replace(" ","-").toLowerCase();
+    database.ref("userdata/"+user.uid+"/projects").child(projectId).set({
         code:"function setup(){\n  //setup code here\n}\n\nfunction draw(){\n  //draw code goes here\n}\n",
         lessonId:"none",
         name:projectName
