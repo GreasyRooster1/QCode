ef4ee51c5b5dc1175165731aecf849a6c1d4baf8
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-04 11:36:00 -0700

    return to page after login

--- global/firebase/lockPageToAuth.js
+++ global/firebase/lockPageToAuth.js
@@ -1,9 +1,10 @@
 
 if(getStoredUser()===null){
+    console.log(window.location.href);
     if(window.location.href.includes("index.html")||window.location.href.endsWith("/")){
-        window.location.replace("login/login.html");
+        window.location.replace("login/login.html?retUrl="+window.location.href);
     }else {
-        window.location.href = "../login/login.html";
+        window.location.href = "../login/login.html?retUrl="+window.location.href;
     }
 }else{
     console.log("authorized!");
