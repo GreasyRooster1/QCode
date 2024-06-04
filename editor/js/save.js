406094c74bd1a08435a1ac25afeb777a682c4339
QuantumDeity <greasyRooster1@gmail.com>
2024-06-03 17:03:01 -0700

    made save alert fade

--- editor/js/save.js
+++ editor/js/save.js
@@ -1,4 +1,7 @@
 const saveButton = document.querySelector('.save-button');
+const saveAlert = document.querySelector('.save-alert');
+
+const saveAlertFadeDuration = .75;
 
 saveButton.addEventListener("click", function() {
     saveCode();
@@ -8,4 +11,16 @@ function saveCode() {
     let code = getCodeFromEditor();
     let user = getStoredUser();
     database.ref("userdata/"+user.uid+"/projects/"+projectId+"/code").set(code);
+    showSaveAlert();
+}
+
+function showSaveAlert(){
+    saveAlert.style.opacity="1";
+    let amountPerFrame = (1/saveAlertFadeDuration)/100;
+    let fadeOutInterval = setInterval(function(){
+        saveAlert.style.opacity=(saveAlert.style.opacity-amountPerFrame).toString();
+        if(saveAlert.style.opacity<=0){
+            clearInterval(fadeOutInterval);
+        }
+    },10)
 }
