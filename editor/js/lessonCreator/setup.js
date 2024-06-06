f663d88fa89e7c721497d64459492c5f6e684c98
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-06 16:19:28 -0700

    add new step button

--- editor/js/lessonCreator/setup.js
+++ editor/js/lessonCreator/setup.js
@@ -1,4 +1,14 @@
+const buttonContainer = document.querySelector(".lesson-creator-new-button-container");
+
 function setupLessonCreator(){
     setupPanes(true);
-    createStep("demo step head","demo step content","https://github.com/GreasyRooster1/QCodeStatic/blob/main/Global/missing.png?raw=true","info");
+    createStep("demo step head","demo step content","https://github.com/GreasyRooster1/QCodeStatic/blob/main/Global/missing.png?raw=true","info","-1");
+
+    createNewStepButton();
+}
+
+function createNewStepButton(){
+    let buttonEl = document.createElement("div")
+    buttonEl.innerHTML = "New Step";
+    buttonContainer.appendChild(buttonEl);
 }
