639aaa769155d1cabf2e761d4d4e8f648066b143
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-07 10:07:03 -0700

    styled lesson links

--- js/loading/lessons.js
+++ js/loading/lessons.js
@@ -11,23 +11,25 @@ function loadLessons(){
 }
 
 function createLessonElement(lessonId,lessonData){
-    let el = document.createElement("div");
-    el.setAttribute("data-lessonid",lessonId);
-    el.addEventListener("click",lessonClickHandle);
+    //check if lesson exists or not
     database.ref("lessons/"+lessonId).once("value").then(function (snap) {
         let data = snap.val();
         if(data.unlisted){
             return;
         }
+        let el = document.createElement("div");
+        el.setAttribute("data-lessonid",lessonId);
+        el.classList.add("lesson-link");
+        el.addEventListener("click",lessonClickHandle);
+        lessonsDisplay.appendChild(el);
         el.innerHTML = data.name;
-    })
-    lessonsDisplay.appendChild(el);
+    });
 }
 
 function lessonClickHandle(e){
-    let link = e.target;
+    let linkEl = e.target;
     let uid = getStoredUser().uid;
-    let lessonId = link.getAttribute("data-lessonid");
+    let lessonId = linkEl.getAttribute("data-lessonid");
     let ref = "userdata/"+uid+"/projects/";
     let projectId = lessonId
     database.ref(ref+projectId).once("value").then(function (snap) {
