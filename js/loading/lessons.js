9dbec1ad37e1536bd5bce9f0cf013a8ecd9b1b40
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-07 09:54:32 -0700

    creating lessons works

--- js/loading/lessons.js
+++ js/loading/lessons.js
@@ -3,10 +3,49 @@ function loadLessons(){
     lessonsRef.on('value', (snapshot) => {
         const data = snapshot.val();
         clearLessons();
+        console.log(data);
         for(const [lessonId, lessonData] of Object.entries(data)){
-            console.log(lessonId,lessonData);
+            createLessonElement(lessonId,lessonData);
         }
     });
+}
+
+function createLessonElement(lessonId,lessonData){
+    let el = document.createElement("div");
+    el.setAttribute("data-lessonid",lessonId);
+    el.addEventListener("click",lessonClickHandle);
+    database.ref("lessons/"+lessonId).once("value").then(function (snap) {
+        let data = snap.val();
+        el.innerHTML = data.name;
+    })
+    lessonsDisplay.appendChild(el);
+}
+
+function lessonClickHandle(e){
+    let link = e.target;
+    let uid = getStoredUser().uid;
+    let lessonId = link.getAttribute("data-lessonid");
+    let ref = "userdata/"+uid+"/projects/";
+    let projectId = lessonId+""
+    database.ref(ref+projectId).once("value").then(function (snap) {
+        if(snap.exists()){
+            window.location.href = "editor/editor.html?projectId="+projectId+"&uid="+uid;
+            return;
+        }
+        database.ref("lessons/"+lessonId).once("value").then(function (snap) {
+            let lessonData = snap.val();
+            let starterCode = lessonData.starterCode;
+            if(starterCode==="default"){
+                starterCode = defaultCode;
+            }
+            database.ref(ref).child(projectId).set({
+                code:starterCode,
+                name:lessonData.name,
+                lessonId: lessonId,
+            });
+            window.location.href = "editor/editor.html?projectId="+projectId+"&uid="+getStoredUser().uid;
+        });
+    })
 }
 
 function clearLessons(){
--- js/newProject.js
+++ js/newProject.js
@@ -1,5 +1,5 @@
 const newProjectButton = document.querySelector('.new-project-button');
-
+const defaultCode = "function setup(){\n  //setup code here\n}\n\nfunction draw(){\n  //draw code goes here\n}\n"
 newProjectButton.addEventListener('click', (e) => {
     createProject(prompt("name for new project"));
 })
@@ -8,7 +8,7 @@ function createProject(projectName){
     let user = getStoredUser();
     let projectId = projectName.replace(" ","-").toLowerCase();
     database.ref("userdata/"+user.uid+"/projects").child(projectId).set({
-        code:"function setup(){\n  //setup code here\n}\n\nfunction draw(){\n  //draw code goes here\n}\n",
+        code:defaultCode,
         lessonId:"none",
         name:projectName
     })
