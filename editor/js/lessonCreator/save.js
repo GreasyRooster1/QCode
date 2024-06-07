a18bc85b00db5666dd32ac2790e3c26c097a9ce1
QuantumDeity <greasyRooster1@gmail.com>
2024-06-06 21:07:13 -0700

    lessons can now be saved

--- editor/js/lessonCreator/save.js
+++ editor/js/lessonCreator/save.js
@@ -13,5 +13,34 @@ function createSaveButton(){
 }
 
 function saveLesson(){
+    for (let step of scrollableSteps.children){
+        let stepData = {
+            head:step.getAttribute("head"),
+            content:step.querySelector(".step-text-content"),
+            image:step.getAttribute("image"),
+            type:step.getAttribute("type"),
+        }
+        addedSteps.push(stepData);
+    }
+    download(JSON.stringify(addedSteps,null,4));
+    addedSteps=[];
+}
 
+
+
+function download(data) {
+    let date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+    const file = new File([data], 'QCode_Lesson'+date+'.json', {
+        type: 'text/plain',
+    })
+    const link = document.createElement('a')
+    const url = URL.createObjectURL(file)
+
+    link.href = url
+    link.download = file.name
+    document.body.appendChild(link)
+    link.click()
+
+    document.body.removeChild(link)
+    window.URL.revokeObjectURL(url)
 }
