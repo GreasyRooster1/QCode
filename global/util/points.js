e886ea635abd3d90412de877216406e957804571
GreasyRooster <greasyRooster1@gmail.com>
2024-06-09 15:28:48 -0700

    added point calculation

---
+++ global/util/points.js
@@ -0,0 +1,26 @@
+function reCalculateUserPoints(){
+    database.ref("userdata/"+getStoredUser().uid+"/badges").once("value", function(snap){
+       let data = snap.val();
+       let totalPoints = 0;
+       let count = 0
+
+       for(let badge of data){
+
+           database.ref("badges/"+badge.id+"/value").once("value", function(snap){
+               totalPoints+=snap.val();
+               console.log(totalPoints,count,data.length);
+               if(count===data.length){
+
+                   setPointValue(totalPoints);
+               }
+           })
+
+           count++;
+       }
+
+    });
+}
+
+function setPointValue(val){
+    database.ref("userdata/"+getStoredUser().uid+"/points").set(val)
+}
