7d290f6352b5c303a92b4d9ac3c477c619489a01
GreasyRooster <greasyRooster1@gmail.com>
2024-06-08 21:45:11 -0700

    links now work with chapters

--- js/loading/projects.js
+++ js/loading/projects.js
@@ -14,7 +14,7 @@ function loadProjects(){
 
 function createProjectElement(projectId,projectData){
     let el = document.createElement("project-link");
-    el.setAttribute("href","editor/editor.html?projectId="+projectId+"&uid="+getStoredUser().uid);
+    el.setAttribute("href",getLinkToProject(projectId,getStoredUser().uid,"auto"));
     el.setAttribute("name",projectData.name);
     projectsDisplay.appendChild(el);
 }
@@ -23,3 +23,10 @@ function clearProjects(){
     projectsDisplay.innerHTML = "";
 }
 
+function openProjectInEditor(projectId,uid,chapterNumber){
+    window.location.href = getLinkToProject(projectId,uid,chapterNumber)
+}
+
+function getLinkToProject(projectId,uid,chapterNumber){
+    return "editor/editor.html?projectId="+projectId+"&uid="+uid+"&cNum="+chapterNumber;
+}
