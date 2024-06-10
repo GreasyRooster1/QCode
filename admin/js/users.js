ea006256a601806f1cffa122b4b2876e25b23197
GreasyRooster <greasyRooster1@gmail.com>
2024-06-09 17:46:03 -0700

    getting user data

--- admin/js/users.js
+++ admin/js/users.js
@@ -1,30 +1,35 @@
 const userDisplay = document.querySelector('.users-display');
 
+const userDetailsName = document.querySelector(".user-details .name");
+const userDetailsUid = document.querySelector(".user-details .uid");
+const userDetailsPoints = document.querySelector(".user-details .points");
+
 function setupUsers(){
     database.ref('userdata').once('value').then((snapshot) => {
         const data = snapshot.val();
         clearUsers();
         console.log(data);
-        for(const [userUid, userData] of Object.entries(data)){
-            createUserElement(userUid,userData);
+        for(const [uid, userData] of Object.entries(data)){
+            createUserElement(uid,userData);
         }
     });
 }
 
-function createUserElement(userUid,userData){
+function createUserElement(uid,userData){
     let userEl = document.createElement("div");
     let wrapperEl = document.createElement("div");
 
     wrapperEl.classList.add("listed-data-item-wrapper");
 
     userEl.classList.add("listed-data-item");
-    userEl.setAttribute("data-userid",userUid);
+    userEl.setAttribute("data-uid",uid);
 
     if(userData.username===undefined){
         userEl.innerHTML = "[Username Not Set]";
     }else {
         userEl.innerHTML = userData.username;
     }
+    userEl.addEventListener("click",showUserDetails);
 
     wrapperEl.appendChild(userEl);
     userDisplay.appendChild(wrapperEl);
@@ -32,6 +37,19 @@ function createUserElement(userUid,userData){
 
 function clearUsers(){
     userDisplay.innerHTML = ""
+}
+
+function showUserDetails(e){
+    let uid = e.currentTarget.getAttribute("data-uid");
+
+    userDetailsUid.innerHTML = uid;
+    database.ref("userdata/"+uid).once("value").then((snap)=>{
+        let data = snap.val();
+
+        userDetailsName.innerHTML = data.username;
+        userDetailsPoints.innerHTML = data.points;
+    });
+
 }
 
 setupUsers();
