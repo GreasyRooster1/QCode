63056ba527db035e671323b06c901c07da0ccdf4
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-11 10:21:56 -0700

    show date as aprox

--- js/loading/announcements.js
+++ js/loading/announcements.js
@@ -20,17 +20,75 @@ function createAnnouncementElement(data){
 
     title.innerHTML = data.title;
     content.innerHTML = data.content;
-    date.innerHTML = data.date;
+    date.innerHTML = timeDifference(data.date*1000);
 
     announcementWrapper.classList.add("announcement")
     title.classList.add("title")
     content.classList.add("content")
     date.classList.add("date")
 
+    date.setAttribute("data-date",data.date)
+    date.addEventListener("mouseover",dateHoverIn);
+    date.addEventListener("mouseout",dateHoverOut);
+
     announcementWrapper.appendChild(title)
     announcementWrapper.appendChild(content)
     announcementWrapper.appendChild(date)
     announcementsDisplay.appendChild(announcementWrapper);
+}
+
+function dateHoverIn(e){
+    e.target.innerHTML = getDateString(e.target.getAttribute("data-date"));
+}
+function dateHoverOut(e){
+    e.target.innerHTML = timeDifference(e.target.getAttribute("data-date")*1000);
+}
+
+function getDateString(unixStamp){
+    let date = new Date(unixStamp * 1000);
+
+    let day = date.getDate();
+    let month = date.getMonth()+1;
+    let year = date.getFullYear();
+
+    return month+"/"+day+"/"+year;
+}
+
+function timeDifference(previous) {
+
+    let msPerMinute = 60 * 1000;
+    let msPerHour = msPerMinute * 60;
+    let msPerDay = msPerHour * 24;
+    let msPerMonth = msPerDay * 30;
+    let msPerYear = msPerDay * 365;
+
+    let current = Date.now()
+
+    let elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+        return Math.round(elapsed/1000) + ' seconds ago';
+    }
+
+    else if (elapsed < msPerHour) {
+        return Math.round(elapsed/msPerMinute) + ' minutes ago';
+    }
+
+    else if (elapsed < msPerDay ) {
+        return Math.round(elapsed/msPerHour ) + ' hours ago';
+    }
+
+    else if (elapsed < msPerMonth) {
+        return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';
+    }
+
+    else if (elapsed < msPerYear) {
+        return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';
+    }
+
+    else {
+        return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';
+    }
 }
 
 function clearAnnouncements(){
