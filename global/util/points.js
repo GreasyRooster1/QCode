d3bb4c9840969599629ea5370bac4f223c6d513c
GreasyRooster <greasyRooster1@gmail.com>
2024-06-10 19:03:40 -0700

    fix badge loading error

--- global/util/points.js
+++ global/util/points.js
@@ -2,10 +2,12 @@ function reCalculateUserPoints(exitFunc){
     database.ref("userdata/"+getStoredUser().uid+"/badges").once("value", function(snap){
        let data = snap.val();
        let totalPoints = 0;
-       let count = 0
+       let count = 0;
 
        for(let badge of data){
-
+           if(badge===undefined){
+               continue;
+           }
            database.ref("badges/"+badge.id+"/value").once("value", function(snap){
                totalPoints+=snap.val();
 
