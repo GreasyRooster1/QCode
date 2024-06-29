8bee2c9b090c0e0e7a3f728854abc02c7ae03010
GreasyRooster <greasyRooster1@gmail.com>
2024-06-29 15:03:10 -0700

    added api bundler

---
+++ js/api/ApiBundler.js
@@ -0,0 +1,26 @@
+let scripts = [
+    "ApiComponent.js",
+    "ComponentRegistry.js",
+]
+
+let components = [
+    "StepComponent.js"
+]
+
+function addScripts(){
+    for(let scriptSrc of scripts){
+        let script = document.createElement("script");
+        script.src = "js/api/"+scriptSrc;
+        script.defer = true;
+        document.head.appendChild(script)
+    }
+
+    for(let componentSrc of components){
+        let script = document.createElement("script");
+        script.src = "js/api/components/"+componentSrc;
+        script.defer = true;
+        document.head.appendChild(script)
+    }
+}
+
+addScripts()
