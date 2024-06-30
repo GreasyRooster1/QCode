b1d6054bea7a8157bc40aaa31b8a9421b7279dd2
GreasyRooster <greasyRooster1@gmail.com>
2024-06-29 23:11:55 -0700

    added user class

--- js/api/firebase/User.js
+++ js/api/firebase/User.js
@@ -1,5 +1,22 @@
 class User{
-    constructor(name){
 
+    createdAt = "";
+    username = "";
+    email = "";
+    lastLoginAt = "";
+    uid = "";
+    permissions = {};
+
+    constructor(firebaseUserObj){
+        this.createdAt = firebaseUserObj.createdAt;
+        this.email = firebaseUserObj.email;
+        this.lastLoginAt = firebaseUserObj.lastLoginAt;
+        this.uid = firebaseUserObj.uid;
+    }
+
+    loadUserPermissions(data){
+        for (let [key,value] of Object.entries(data)){
+            this.permissions[key] = value;
+        }
     }
 }
