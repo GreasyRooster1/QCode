391a4c4f7566ba223baadbd5be05766eb6dfd0ab
GreasyRooster <greasyRooster1@gmail.com>
2024-06-29 21:58:11 -0700

    made component registering work with names

--- js/api/ComponentRegistry.js
+++ js/api/ComponentRegistry.js
@@ -3,7 +3,7 @@ class ComponentRegistry {
     static componentRegistry = [];
 
     static registerHtmlComponent(component) {
-        customElements.define(component.name, component);
+        customElements.define(component.name+"-component", component);
         let instance = new component()
         this.componentRegistry.push(instance);
         instance.onRegister();
