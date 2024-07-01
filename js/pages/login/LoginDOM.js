8f8f1635778d461223bf4930b10375d8cc474643
GreasyRooster <greasyRooster1@gmail.com>
2024-07-01 15:15:32 -0700

    added login api

---
+++ js/pages/login/LoginDOM.js
@@ -0,0 +1,8 @@
+class LoginDOM{
+    static loginButton = document.querySelector(".login-button");
+    static usernameInput = document.querySelector(".username-input");
+    static passwordInput = document.querySelector(".password-input");
+
+    static authErrorDisplayWrapper = document.querySelector(".auth-error");
+    static authErrorContent = document.querySelector(".auth-error-message");
+}
