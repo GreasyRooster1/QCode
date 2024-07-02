595ff7f3fd76c052872282a6724e3f43650e1ba9
GreasyRooster <greasyRooster1@gmail.com>
2024-07-01 17:41:12 -0700

    fix dom vars

--- js/pages/login/LoginDOM.js
+++ js/pages/login/LoginDOM.js
@@ -1,7 +1,16 @@
 class LoginDOM{
-    static usernameInput = document.querySelector(".username-input");
-    static passwordInput = document.querySelector(".password-input");
+    static usernameInput;
+    static passwordInput;
 
-    static authErrorDisplayWrapper = document.querySelector(".auth-error");
-    static authErrorContent = document.querySelector(".auth-error-message");
+    static authErrorDisplayWrapper;
+    static authErrorContent;
+
+    static getElements(){
+        LoginDOM.usernameInput = document.querySelector(".username-input");
+        LoginDOM.passwordInput = document.querySelector(".password-input");
+        LoginDOM.authErrorDisplayWrapper = document.querySelector(".auth-error");
+        LoginDOM.authErrorContent = document.querySelector(".auth-error-message");
+    }
 }
+
+LoadRegistry.register(LoginDOM.getElements);
