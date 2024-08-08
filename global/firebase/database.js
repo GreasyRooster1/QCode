b730f380afc1594ebd823b202fb68d1ace50416e
GreasyRooster <greasyRooster1@gmail.com>
2024-08-07 17:17:06 -0700

    give bouncing rainbow squares as default project

--- global/firebase/database.js
+++ global/firebase/database.js
@@ -1,6 +1,6 @@
 const database = firebase.database();
 const userdataFolder = database.ref('userdata');
 
-function setDarkMode(dark){
-    userdataFolder.child(getStoredUser().uid).child("darkmode").set(dark);
+function validateUserData(){
+
 }
--- login/js/login.js
+++ login/js/login.js
@@ -6,7 +6,15 @@ const authErrorDisplayWrapper = document.querySelector(".auth-error");
 const authErrorContent = document.querySelector(".auth-error-message");
 
 const requiredUserData = [
-    {name:"projects",val:{}},
+    {name:"projects",val:{
+        "bouncing-rainbow-squares":{
+            name:"Bouncing Rainbow Squares",
+            code:"\n\nfunction setup() {\n\n}\n\nfunction draw() {\n\n}\n",
+            currentChapter:0,
+            currentStep:1,
+            lessonId:"bouncing-rainbow-squares",
+        }
+    }},
     {name:"badges",val:{0:{id:"user"}}},
     {name:"points",val:0},
     {name:"spentPoints",val:0},
