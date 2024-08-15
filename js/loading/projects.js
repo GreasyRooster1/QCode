52e9d8db5fee5984ac62875095cbd682189bcf92
GreasyRooster <greasyRooster1@gmail.com>
2024-08-15 10:09:19 -0700

    fixed

--- js/loading/projects.js
+++ js/loading/projects.js
@@ -1,19 +1,14 @@
 let currentProjectViewPage = 1;
 
 function loadProjects(){
-    let projectsRef = database.ref('userdata/'+user.uid+"/projects").orderByChild("timestamp").limitToLast(currentProjectViewPage*5);
+    let projectsRef = database.ref('userdata/'+user.uid+"/projects").orderByChild("timestamp")
     projectsRef.on('value', (snapshot) => {
         clearProjects();
         let i=0;
         snapshot.forEach((childSnapshot) => {
-            if(i>=(currentProjectViewPage-1)*5&&i<currentProjectViewPage*5) {
-                const data = childSnapshot.val();
-                console.log("loaded project " + childSnapshot.key + " " + i);
-                createProjectElement(childSnapshot.key, data);
-            }else {
-                console.log("didnt load project " + childSnapshot.key + " " + i);
-            }
-            i++;
+            const data = childSnapshot.val();
+            console.log("loaded project " + childSnapshot.key + " " + i);
+            createProjectElement(childSnapshot.key, data);
         });
     });
 }
@@ -23,7 +18,7 @@ function createProjectElement(projectId,projectData){
     el.setAttribute("href",getLinkToProject(projectId,getStoredUser().uid,projectData.currentChapter));
     el.setAttribute("name",projectData.name);
     el.setAttribute("timestamp",projectData.timestamp);
-    projectsDisplay.append(el);
+    projectsDisplay.prepend(el);
 }
 
 function clearProjects(){
@@ -36,21 +31,4 @@ function openProjectInEditor(projectId,uid,chapterNumber){
 
 function getLinkToProject(projectId,uid,chapterNumber){
     return "editor/editor.html?projectId="+projectId+"&uid="+uid+"&cNum="+chapterNumber;
-}
-
-function updateProjectPageDisplay(){
-    document.querySelector(".project-pages > .page-display").innerHTML = currentProjectViewPage;
-    loadProjects()
-}
-
-document.querySelector(".project-pages > .left-arrow").onclick = function(){
-    if(currentProjectViewPage > 1){
-        currentProjectViewPage--;
-        updateProjectPageDisplay()
-    }
-}
-
-document.querySelector(".project-pages > .right-arrow").onclick = function(){
-    currentProjectViewPage++;
-    updateProjectPageDisplay()
 }
