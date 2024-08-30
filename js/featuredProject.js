3bb2f366a7cd3186b6dee48583a9bd18494d8f1d
GreasyRooster <greasyRooster1@gmail.com>
2024-08-29 18:31:25 -0700

    fix featured project

--- js/featuredProject.js
+++ js/featuredProject.js
@@ -2,7 +2,7 @@ let featuredProjectData = {};
 const frame = document.querySelector('#exec-frame');
 
 
-database.ref("userdata/"+getStoredUser().uid+"/projects").limitToLast(1).once("value").then(function(snapshot) {
+database.ref("userdata/"+getStoredUser().uid+"/projects").orderByChild('timestamp').limitToLast(1).once("value").then(function(snapshot) {
     featuredProjectData = Object.entries(snapshot.val())[0][1];
 })
 
