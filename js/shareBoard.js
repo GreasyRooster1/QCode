62d5923475122185ba2ba08d9efd0e9fc05713fc
GreasyRooster <greasyRooster1@gmail.com>
2024-10-12 16:43:44 -0700

    fill icons

--- js/shareBoard.js
+++ js/shareBoard.js
@@ -4,6 +4,9 @@ let currentFeaturedProject = 0;
 let heartInteractCount = document.querySelector(".heart-count");
 let starInteractCount = document.querySelector(".star-count");
 
+let heartInteractIcon = document.querySelector(".heart-icon>i");
+let starInteractIcon = document.querySelector(".star-icon>i");
+
 
 function initShareBoard(){
     getShareBoardFeaturedProjects(function(projects){
@@ -14,8 +17,35 @@ function initShareBoard(){
 
 function setCurrentFeaturedProject(index){
     currentFeaturedProject = index;
-    console.log(featuredProjects[currentFeaturedProject])
+
     heartInteractCount.innerHTML = Object.keys(featuredProjects[currentFeaturedProject].likedBy).length;
     starInteractCount.innerHTML = Object.keys(featuredProjects[currentFeaturedProject].staredBy).length;
 
+    checkHeartInteractFilled()
+    checkStarInteractFilled()
+}
+
+function checkHeartInteractFilled(){
+    let currentProject = featuredProjects[currentFeaturedProject];
+    heartInteractIcon.classList.remove("far");
+    heartInteractIcon.classList.remove("fas");
+    if(currentProject.likedBy.hasOwnProperty(getStoredUser().uid)){
+        console.log("asd");
+        heartInteractIcon.classList.add("fas");
+    }else{
+        heartInteractIcon.classList.add("far");
+    }
+}
+
+function checkStarInteractFilled(){
+    let currentProject = featuredProjects[currentFeaturedProject];
+    starInteractIcon.classList.remove("far");
+    starInteractIcon.classList.remove("fas");
+    if(currentProject.staredBy.hasOwnProperty(getStoredUser().uid)){
+        console.log("asd");
+
+        starInteractIcon.classList.add("fas");
+    }else{
+        starInteractIcon.classList.add("far");
+    }
 }
