0b1d8f98e1264210ab1c92298c82a2147d51b195
GreasyRooster <greasyRooster1@gmail.com>
2024-10-13 13:48:06 -0700

    create elements

---
+++ shareBoard/js/featured.js
@@ -0,0 +1,55 @@
+class FeaturedProject{
+    constructor(projectData){
+        this.projectMetadata = projectData;
+        this.createDomElement();
+        this.appendToFeaturedBar()
+    }
+
+    createDomElement(){
+        this.domElement = document.createElement("div");
+
+        this.iframe = document.createElement("iframe");
+        this.interactTray = document.createElement("div");
+
+        this.heartWrapper = document.createElement("span");
+        this.heartIcon = document.createElement("span");
+        this.heartCount = document.createElement("span");
+        this.heartWrapper.appendChild(this.heartIcon);
+        this.heartWrapper.appendChild(this.heartCount);
+
+        this.starWrapper = document.createElement("span");
+        this.starIcon = document.createElement("span");
+        this.starCount = document.createElement("span");
+        this.starWrapper.appendChild(this.starIcon);
+        this.starWrapper.appendChild(this.starCount);
+
+        this.interactTray.appendChild(this.heartWrapper);
+        this.interactTray.appendChild(this.starWrapper);
+
+        this.domElement.appendChild(this.iframe);
+        this.domElement.appendChild(this.interactTray);
+
+
+        this.iframe.src = "../editor/exec/exec.html";
+
+        this.interactTray.classList.add("interact-tray");
+        this.heartWrapper.classList.add("hearts-wrapper");
+        this.heartCount.classList.add("heart-count");
+        this.heartCount.classList.add("interaction-count");
+        this.heartIcon.classList.add("heart-icon");
+
+        this.starWrapper.classList.add("star-wrapper");
+        this.starCount.classList.add("star-count");
+        this.starCount.classList.add("interaction-count");
+        this.starIcon.classList.add("star-icon");
+
+        this.heartIcon.innerHTML = "<i class='far fa-heart'></i>";
+        this.starIcon.innerHTML = "<i class='far fa-star'></i>";
+    }
+
+    appendToFeaturedBar(){
+        document.querySelector(".featured-project-bar").appendChild(this.domElement);
+    }
+}
+
+new FeaturedProject(0);
