59af7aabef3912ca7ff9ec7776076574d0f1c3f7
GreasyRooster <greasyRooster1@gmail.com>
2024-10-14 18:17:09 -0700

    remove nav arrow

--- shareBoard/js/index.js
+++ shareBoard/js/index.js
@@ -1,5 +1,11 @@
 function init(){
     initFeaturedBar();
+    removeNavArrow();
+}
+
+function removeNavArrow(){
+    let arrow = document.querySelector(".navbar-visibility-button");
+    arrow.remove();
 }
 
 init();
