18041346499e15bbff4cc2e1769096ebaddd3985
GreasyRooster <greasyRooster1@gmail.com>
2024-10-24 18:53:45 -0700

    fix

--- editor/js/share.js
+++ editor/js/share.js
@@ -1,7 +1,7 @@
 const shareButton = document.querySelector('.share-button');
 const popupContainer = document.querySelector('.share-popup-container');
 const sharePopupButton = document.querySelector('.share-popup-button');
-const shareNameInput = document.querySelector('.share-name-input');
+const shareNameInput = document.querySelector('.share-popup-content .name-input');
 const previewIframe = document.getElementById('share-preview-frame');
 
 shareButton.addEventListener('click', (e) => {
