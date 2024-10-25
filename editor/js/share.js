9cc349790c6503b873d83c51452ef052d415e9b0
GreasyRooster <greasyRooster1@gmail.com>
2024-10-24 17:37:35 -0700

    show popup

--- editor/js/share.js
+++ editor/js/share.js
@@ -1,5 +1,12 @@
 const shareButton = document.querySelector('.share-button');
+const popupContainer = document.querySelector('.share-popup-container');
 
 shareButton.addEventListener('click', (e) => {
     rawSave();
+    showPopup();
 });
+
+function showPopup(){
+    popupContainer.style.opacity = "1";
+    popupContainer.style.pointerEvents = "auto";
+}
