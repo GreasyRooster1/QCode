f38e8fbe8c4f9783f273882cb68fb5de79234fdc
GreasyRooster <greasyRooster1@gmail.com>
2024-10-24 18:39:58 -0700

    run preview code

--- editor/js/share.js
+++ editor/js/share.js
@@ -1,12 +1,28 @@
 const shareButton = document.querySelector('.share-button');
 const popupContainer = document.querySelector('.share-popup-container');
+const sharePopupButton = document.querySelector('.share-popup-button');
+const previewIframe = document.getElementById('share-preview-frame');
 
 shareButton.addEventListener('click', (e) => {
     rawSave();
     showPopup();
+    runPopupPreviewCode();
 });
 
 function showPopup(){
     popupContainer.style.opacity = "1";
     popupContainer.style.pointerEvents = "auto";
 }
+
+function runPopupPreviewCode(){
+    previewIframe.contentWindow.location.reload();
+
+    previewIframe.addEventListener("load", () => {
+        previewIframe.contentWindow.postMessage(getCodeFromEditor())
+    })
+}
+
+
+sharePopupButton.addEventListener('click', (e) => {
+
+})
