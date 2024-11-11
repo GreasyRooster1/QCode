f78a51aa33be41a8689ef5cede6813f6c19e9c14
GreasyRooster <greasyRooster1@gmail.com>
2024-11-10 16:01:52 -0800

    info

--- shareBoard/js/project/project.js
+++ shareBoard/js/project/project.js
@@ -9,11 +9,24 @@ function loadProject() {
     const shareBoardID = urlParams.get('shareboardid');
     database.ref("sharedProjects/metadata/"+shareBoardID).once("value").then((snapshot) => {
         projectMetadata = snapshot.val();
+        insertInfo()
     })
     database.ref("sharedProjects/projectData/"+shareBoardID).once("value").then((snapshot) => {
         projectCode = snapshot.val();
         execFrame.contentWindow.location.reload();
     })
+}
+
+function insertInfo(){
+    let title = document.querySelector(".info-title");
+    let date = document.querySelector(".info-date");
+    let authorImg = document.querySelector(".info-author.author-icon img");
+    let authorUsername = document.querySelector(".info-author.author-username");
+    let desc = document.querySelector(".info-desc");
+
+    title.innerText = projectMetadata.name;
+    desc.innerText = projectMetadata.desc;
+    date.innerText = projectMetadata.timestamp;
 }
 
 function setupFrame(){
