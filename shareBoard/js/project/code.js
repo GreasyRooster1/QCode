fd4bd28beae394a4f0c5505b310a42672cd23138
GreasyRooster <greasyRooster1@gmail.com>
2024-11-10 22:15:23 -0800

    asdasd

---
+++ shareBoard/js/project/code.js
@@ -0,0 +1,35 @@
+import { basicSetup, EditorView} from "codemirror/codemirror/dist/index.js"
+import { keymap } from "codemirror/view/dist/index.js"
+import { javascript } from "codemirror/lang-javascript/dist/index.js"
+import { indentWithTab } from "codemirror/commands/dist/index.js"
+import { linter } from 'codemirror/lint/dist/index.js'
+import { javascriptLanguage } from 'codemirror/lang-javascript/dist/index.js'
+
+const customTheme = EditorView.theme({
+    '&': {
+        font:"'JetBrains Mono', monospace",
+        fontSize: "15px",
+        fontVariantLigatures:"none",
+    }
+})
+
+let editorView = new EditorView({
+    doc: "\n",
+    extensions: [
+        basicSetup,
+        javascript(),
+        linter(javascriptLanguage),
+        keymap.of([indentWithTab]),
+        customTheme,
+    ],
+    updateListener:onDocUpdate,
+    parent: document.querySelector(".code-editor"),
+});
+
+function onDocUpdate(){
+    hasSavedRecently = false;
+}
+
+window.editor = editorView;
+
+export const editor=editorView;
