ddaf628a3cf8d73c18c0f556f355b8fd805dc680
GreasyRooster <greasyRooster1@gmail.com>
2024-12-14 15:29:05 -0800

    data url

--- lessons/js/index.js
+++ lessons/js/index.js
@@ -13,4 +13,19 @@ function setupButtons(){
         swapColors()
     })
 }
+
+function toDataURL(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            callback(reader.result);
+        }
+        reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+}
+
 init();
