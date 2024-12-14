034c0a33ccb7b0c5f5012df34e0c4c1e2fca7af6
GreasyRooster <greasyRooster1@gmail.com>
2024-12-13 21:37:38 -0800

    icons working

--- lessons/js/index.js
+++ lessons/js/index.js
@@ -1,5 +1,17 @@
 function init(){
     removeNavArrow()
+    setupButtons()
+}
+
+function setupButtons(){
+    document.querySelector(".recenter").addEventListener('click', function(){
+        camera.zoom = 1;
+        camera.x = 0;
+        camera.y = 0;
+    })
+    document.querySelector(".dark-mode").addEventListener('click', function(){
+        swapColors()
+    })
 }
 
 init();
