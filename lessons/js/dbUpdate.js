be46d83b45e01e7da3643aa3e755a0f7ac0153ec
GreasyRooster <greasyRooster1@gmail.com>
2024-12-18 09:48:47 -0800

    update db

--- lessons/js/dbUpdate.js
+++ lessons/js/dbUpdate.js
@@ -32,7 +32,7 @@ function updateAllDb(){
 function updateLessonRecommendationsDb() {
     let recs = []
     searchForRecommendation(rootLesson,10,recs)
-
+    database.ref("userdata/"+getStoredUser().uid+"/recommendedLessons").set(recs)
 }
 
 function searchForRecommendation(lessonId,depth,recs){
