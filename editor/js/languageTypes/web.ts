434e12061621dc0582bdb9354d8a964730d45540
GreasyRooster <greasyRooster1@gmail.com>
2024-12-27 12:47:50 -0800

    remove stuff

--- editor/js/languageTypes/web.ts
+++ editor/js/languageTypes/web.ts
@@ -9,20 +9,9 @@ class WebType extends ProjectType {
     }
 
     onLoad(){
-        writeToEditor(this.projectData!["code"]);
     }
 
     saveCode(){
-        let code = getCode();
-        let user = getStoredUser();
-        database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/code").set(code);
-        database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/dateUpdated").set(Date.now()/1000);
-        if(this.hasLesson) {
-            console.log(this.highestViewedStep)
-            database.ref("userdata/" + user.uid + "/projects/" + this.projectId + "/currentStep").set(this.highestViewedStep);
-            database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/currentChapter").set(this.currentChapter);
-        }
-
     }
 
     run(errorCallback:RunErrCallback) {
@@ -32,16 +21,6 @@ class WebType extends ProjectType {
     }
 
     runErrorCallback(content: string, type: string): void {
-        let logEl = document.createElement("console-log");
-        let consoleOut = document.querySelector(".console-output-pane")
-        while(consoleOut!.children.length > 100){
-            consoleOut!.children[consoleOut!.childElementCount-1].remove()
-        }
-        logEl.setAttribute("type", type);
-        logEl.setAttribute("message", content);
-        logEl.setAttribute("head", logNames[type]);
-        consoleOut!.insertBefore(logEl,consoleOut!.firstChild);
-
     }
 
     getLanguage():string {
