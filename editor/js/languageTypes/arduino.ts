93a29dcb39e7135cdec0bfcd8d9fc874fba53150
GreasyRooster <greasyRooster1@gmail.com>
2025-01-16 08:57:46 -0800

    arduino type

--- editor/js/languageTypes/arduino.ts
+++ editor/js/languageTypes/arduino.ts
@@ -1,1 +1,80 @@
 //arduino.work
+
+import {ProjectType, RunErrCallback} from "./projectType";
+import {
+    frame,
+    frameContent,
+    getCode,
+    logNames,
+    runCode,
+    setupEvents as setupExecEvents,
+    stopFrame
+} from "../executionHelper";
+import {Language} from "../codeEditor";
+
+class ArduinoType extends ProjectType {
+    constructor() {
+        super(false);
+    }
+
+    setupEditor(): void {
+        setupExecEvents(()=>{
+            runCode(getCode())
+        },this.runErrorCallback);
+    }
+
+    onLoad(){
+        writeToEditor(this.projectData!["code"]);
+    }
+
+    saveCode(){
+        let code = getCode();
+        let user = getStoredUser();
+        database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/code").set(code);
+        database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/dateUpdated").set(Date.now()/1000);
+        if(this.hasLesson) {
+            console.log(this.highestViewedStep)
+            database.ref("userdata/" + user.uid + "/projects/" + this.projectId + "/currentStep").set(this.highestViewedStep);
+            database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/currentChapter").set(this.currentChapter);
+        }
+
+    }
+
+    run(errorCallback:RunErrCallback) {
+        console.log(frameContent)
+        if(frameContent==undefined){
+            frame?.contentWindow?.location?.reload()
+        }
+        frameContent?.location.reload()
+        clearConsole()
+    }
+
+    stop(){
+        stopFrame();
+        clearConsole()
+    }
+
+    runErrorCallback(content: string, type: string): void {
+        let logEl = document.createElement("console-log");
+        let consoleOut = document.querySelector(".console-output-pane")
+        while(consoleOut!.children.length > 100){
+            consoleOut!.children[consoleOut!.childElementCount-1].remove()
+        }
+        logEl.setAttribute("type", type);
+        logEl.setAttribute("message", content);
+        logEl.setAttribute("head", logNames[type]);
+        consoleOut!.insertBefore(logEl,consoleOut!.firstChild);
+
+    }
+
+    getLanguage():Language {
+        return "javascript";
+    }
+}
+
+function clearConsole(){
+    let consoleOut = document.querySelector(".console-output-pane")
+    consoleOut!.innerHTML = "";
+}
+
+export {ArduinoType};
