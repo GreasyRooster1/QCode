681a296e7480dd89b39e82edb431f9bc95b1f9dc
GreasyRooster <greasyRooster1@gmail.com>
2025-01-18 14:56:01 -0800

    points db refactor

--- src/api/util/points.js
+++ src/api/util/points.js
@@ -1,8 +1,9 @@
 import {db} from "../firebase";
 import {getStoredUser} from "../auth";
+import {get, ref} from "firebase/database";
 
 function reCalculateUserPoints(exitFunc){
-    db.ref("userdata/"+getStoredUser().uid+"/badges").once("value", function(snap){
+    get(ref(db,"userdata/"+getStoredUser().uid+"/badges")).then((snap)=>{
         let data = snap.val();
         let badges = data.values().toArray().filter(b => b!==undefined);
         let totalPoints = 0;
@@ -11,7 +12,7 @@ function reCalculateUserPoints(exitFunc){
        console.log(data)
 
         for(let badge of badges){
-            db.ref("badges/"+badge.id+"/value").once("value", function(snap){
+            get(ref(db,"badges/"+badge.id+"/value")).then((snap)=>{
                 totalPoints+=snap.val();
                 console.log(count,badges.length)
                 if(count===badges.length){
