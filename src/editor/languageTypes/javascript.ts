7124b46891dbbbcdbcf778f9cb19938197966507
GreasyRooster <greasyRooster1@gmail.com>
2025-01-19 14:22:08 -0800

    refactor db for javascript.ts

--- src/editor/languageTypes/javascript.ts
+++ src/editor/languageTypes/javascript.ts
@@ -1,6 +1,9 @@
-import {ProjectType,RunErrCallback} from "./projectType.js";
-import {getCode,setupEvents as setupExecEvents,logNames,runCode,frameContent,frame,stopFrame} from "../../executionHelper.js"
-import {Language} from "../../codeEditor";
+import {ProjectType,RunErrCallback} from "./projectType";
+import {getCode,setupEvents as setupExecEvents,logNames,runCode,frameContent,frame,stopFrame} from "../executionHelper"
+import {Language} from "../codeEditor";
+import {getStoredUser} from "../../api/auth";
+import {ref, set} from "firebase/database";
+import {db} from "../../api/firebase";
 
 class JavascriptType extends ProjectType {
     constructor() {
@@ -20,12 +23,12 @@ class JavascriptType extends ProjectType {
     saveCode(){
         let code = getCode();
         let user = getStoredUser();
-        database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/code").set(code);
-        database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/dateUpdated").set(Date.now()/1000);
+        set(ref(db,"userdata/"+user.uid+"/projects/"+this.projectId+"/code"),code);
+        set(ref(db,"userdata/"+user.uid+"/projects/"+this.projectId+"/dateUpdated"),Date.now()/1000);
         if(this.hasLesson) {
             console.log(this.highestViewedStep)
-            database.ref("userdata/" + user.uid + "/projects/" + this.projectId + "/currentStep").set(this.highestViewedStep);
-            database.ref("userdata/"+user.uid+"/projects/"+this.projectId+"/currentChapter").set(this.currentChapter);
+            set(ref(db,"userdata/" + user.uid + "/projects/" + this.projectId + "/currentStep"),this.highestViewedStep);
+            set(ref(db,"userdata/"+user.uid+"/projects/"+this.projectId+"/currentChapter"),this.currentChapter);
         }
 
     }
