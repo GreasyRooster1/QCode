edcf347106b205e6c6c9498f6261fbcddcb68af9
GreasyRooster <greasyRooster1@gmail.com>
2025-02-20 14:41:37 -0800

    change some architecture

--- src/api/theme.js
+++ src/api/theme.js
@@ -1,26 +1,31 @@
-import {ref,set} from "firebase/database";
+import {onValue, ref, set} from "firebase/database";
 import {getStoredUser} from "./auth";
 import {db} from "./firebase";
 
 
 function loadTheme(){
-    get(ref(db,"userdata/"+getStoredUser().uid+"/theme")).on((snap)=>{
+    onValue(ref(db,"userdata/"+getStoredUser().uid+"/theme"),(snap)=>{
         if(!snap.exists()){
-            set(ref(db,"userdata/"+getStoredUser().uid+"/theme"),{
-                name:"default",
-                external:false,
-            });
+            set(ref(db,"userdata/"+getStoredUser().uid+"/theme"),"default");
             return
         }
-        let data = snap.val();
-
+        let themeId = snap.val();
+        get(ref(db,"themes/"+themeId)).then()
+        setPageTheme(theme)
     })
 }
 
 function setPageTheme(theme){
     let styleEl = document.createElement("link");
-
+    styleEl.rel = "stylesheet";
+    styleEl.href =
     document.head.appendChild(document.createElement('style'));
+}
+
+function getThemeAddress(theme){
+    if(theme.external){
+        return name
+    }
 }
 
 export {loadTheme}
