29ba1f76b34e2d2d34ccf01f607e7906b5ac6be6
GreasyRooster <greasyRooster1@gmail.com>
2025-02-20 14:37:54 -0800

    load theme

--- src/api/theme.js
+++ src/api/theme.js
@@ -1,12 +1,26 @@
-import {ref} from "firebase/database";
+import {ref,set} from "firebase/database";
 import {getStoredUser} from "./auth";
 import {db} from "./firebase";
 
 
 function loadTheme(){
     get(ref(db,"userdata/"+getStoredUser().uid+"/theme")).on((snap)=>{
+        if(!snap.exists()){
+            set(ref(db,"userdata/"+getStoredUser().uid+"/theme"),{
+                name:"default",
+                external:false,
+            });
+            return
+        }
+        let data = snap.val();
 
     })
+}
+
+function setPageTheme(theme){
+    let styleEl = document.createElement("link");
+
+    document.head.appendChild(document.createElement('style'));
 }
 
 export {loadTheme}
