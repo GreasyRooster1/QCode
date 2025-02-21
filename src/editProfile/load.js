9c0eec8f2abb96d7ed0fbd1b885d363aa78f3cef
GreasyRooster <greasyRooster1@gmail.com>
2025-02-20 17:56:44 -0800

    load themes

--- src/editProfile/load.js
+++ src/editProfile/load.js
@@ -5,6 +5,7 @@ import {getStoredUser} from "../api/auth";
 function loadUserData(){
     loadUsername()
     loadProfileIcon()
+    loadTheme()
 }
 
 function loadUsername(){
@@ -16,6 +17,26 @@ function loadUsername(){
 function loadProfileIcon(){
     get(ref(db,"userdata/"+getStoredUser().uid+"/profileIcon")).then((snap)=>{
         document.querySelector(".profile-icon-img").setAttribute("src",snap.val());
+    })
+}
+
+function loadTheme(){
+    get(ref(db,"userdata/"+getStoredUser().uid+"/profileIcon")).then((snap)=> {
+        let themes = snap.val();
+        for(let [key,value] of Object.entries(themes)){
+            let el = document.createElement("option");
+            el.innerHTML = value.name;
+            el.value = key;
+            document.getElementById("themes").appendChild(el);
+        }
+    }).then(()=>{
+        get(ref(db,"userdata/"+getStoredUser().uid+"/profileIcon")).then((snap)=> {
+            if(snap.val()==="default"){
+                return;
+            }
+            let drop = document.getElementById("themes");
+            drop.value = snap.val();
+        });
     })
 }
 
