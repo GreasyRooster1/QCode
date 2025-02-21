34156041f042633c5d37633945a1de0fc33db2d8
GreasyRooster <greasyRooster1@gmail.com>
2025-02-20 17:44:54 -0800

    load profile

--- src/editProfile/load.js
+++ src/editProfile/load.js
@@ -4,11 +4,18 @@ import {getStoredUser} from "../api/auth";
 
 function loadUserData(){
     loadUsername()
+    loadProfileIcon()
 }
 
 function loadUsername(){
     get(ref(db,"userdata/"+getStoredUser().uid+"/username")).then((snap)=>{
         document.querySelector(".profile-username").innerHTML = snap.val();
+    })
+}
+
+function loadProfileIcon(){
+    get(ref(db,"userdata/"+getStoredUser().uid+"/profileIcon")).then((snap)=>{
+        document.querySelector(".profile-icon-img").setAttribute("src",snap.val());
     })
 }
 
