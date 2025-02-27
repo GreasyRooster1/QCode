65f839a0e3add91d23d788368319e494dc746636
GreasyRooster <greasyRooster1@gmail.com>
2025-02-27 09:03:03 -0800

    load fix

--- src/editProfile/index.js
+++ src/editProfile/index.js
@@ -1,6 +1,7 @@
 import {loadTheme} from "../api/theme";
 import {lockPageToAuth} from "../api/util/lockPageToAuth";
 import {initNavbar} from "../api/nav/navbar";
+import {loadUserData} from "./load";
 
 function init() {
     lockPageToAuth()
