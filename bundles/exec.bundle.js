e74cd325d1ff423176a5881d584dd2b75a8e7c5c
GreasyRooster <greasyRooster1@gmail.com>
2025-03-19 15:36:33 -0700

    Updates

--- bundles/exec.bundle.js
+++ bundles/exec.bundle.js
@@ -1,2 +1,32 @@
-(()=>{let parent=null;const acceptedFunctions=["mousePressed","mouseReleased","mouseClicked","mouseMoved","mouseDragged","doubleClicked","mouseWheel","keyPressed","keyReleased","keyTyped","touchStarted","touchEnded"],oldLog=console.log,oldWarn=console.warn,oldErr=console.error;function logMessage(e,...n){if(null===parent)return;let t={type:e,message:n.join(" ")};parent.postMessage(JSON.stringify(t))}function runJs(js){eval(js);let eventFunctions=[];for(let acceptedFunc of acceptedFunctions){let funcDef;try{funcDef=eval(acceptedFunc)}catch(e){funcDef=void 0}void 0!==funcDef&&eventFunctions.push(funcDef)}void 0!==draw&&void 0!==setup&&startP5(draw,setup,eventFunctions)}function startP5(e,n,t){window.setup=function(){createCanvas(500,500),createCanvas=function(){console.error("createCanvas is disabled")},document.getElementById("defaultCanvas0").style.width="100vmin",document.getElementById("defaultCanvas0").style.height="100vmin",n()};for(let e of t)acceptedFunctions.includes(e.name)&&(window[e.name]=e);window.draw=e,new p5}function __canvasTest(){background(0),stroke(255);for(let e=0;e<500;e+=10){let n=(frameCount%500-e)/490,t=e/500;fill(255*n,0,255*t),rect(490*n,0,10,10),fill(0,255*n,255*t),rect(0,490*n,10,10)}fill(255),ellipse(mouseX,mouseY,10,10),textAlign(CENTER),text("Welcome back",250,250)}console.log=function(...e){logMessage("log",e)},console.warn=function(...e){logMessage("warn",e)},console.error=function(...e){logMessage("error",e)},window.onerror=function(e){logMessage("error",e)},window.addEventListener("message",(({data:e,source:n})=>{null===parent&&(parent=n),runJs(e)})),document.addEventListener("contextmenu",(e=>{e.preventDefault()}))})();
-//# sourceMappingURL=exec.bundle.js.map
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/exec/index.js":
+/*!***************************!*\
+  !*** ./src/exec/index.js ***!
+  \***************************/
+/***/ (() => {
+
+eval("\r\nlet parent = null;\r\n\r\nconst acceptedFunctions = [\r\n    \"mousePressed\",\r\n    \"mouseReleased\",\r\n    \"mouseClicked\",\r\n    \"mouseMoved\",\r\n    \"mouseDragged\",\r\n    \"doubleClicked\",\r\n    \"mouseWheel\",\r\n\r\n    \"keyPressed\",\r\n    \"keyReleased\",\r\n    \"keyTyped\",\r\n\r\n    \"touchStarted\",\r\n    \"touchEnded\",\r\n]\r\n\r\nconst oldLog = console.log;\r\nconst oldWarn = console.warn;\r\nconst oldErr = console.error;\r\n\r\nconsole.log = function (...args) {\r\n    logMessage(\"log\",args);\r\n}\r\n\r\nconsole.warn = function (...args) {\r\n    logMessage(\"warn\",args);\r\n}\r\n\r\nconsole.error = function (...args) {\r\n    logMessage(\"error\",args);\r\n}\r\n\r\n//log errors\r\nwindow.onerror = function(error) {\r\n    logMessage(\"error\",error);\r\n}\r\n\r\nfunction logMessage(type,...args){\r\n    if (parent === null) {\r\n        return;\r\n    }\r\n    let log = {type:type,message:args.join(\" \")};\r\n    parent.postMessage(JSON.stringify(log));\r\n}\r\n\r\n\r\nwindow.addEventListener(\"message\", ({ data, source }) => {\r\n    if (parent === null) {\r\n        parent = source;\r\n    }\r\n\r\n    runJs(data);\r\n});\r\n\r\ndocument.addEventListener('contextmenu', event => {\r\n    event.preventDefault();\r\n});\r\n\r\n\r\n\r\nfunction runJs(js){\r\n    //clear dangerous objects and run code\r\n    eval(js);\r\n\r\n    let eventFunctions = [];\r\n\r\n    for (let acceptedFunc of acceptedFunctions){\r\n        let funcDef;\r\n        try {\r\n            funcDef = eval(acceptedFunc);\r\n        }catch(e){\r\n            funcDef = undefined;\r\n        }\r\n\r\n        if (funcDef !== undefined) {\r\n            eventFunctions.push(funcDef);\r\n        }\r\n    }\r\n\r\n    if(draw===undefined||setup===undefined){\r\n        return;\r\n    }\r\n\r\n    startP5(draw,setup,eventFunctions);\r\n}\r\n\r\n\r\n//helpers\r\n\r\n\r\nfunction startP5(drawArg,setupArg,otherFunctions) {\r\n    window.setup = function(){\r\n        createCanvas(500,500);\r\n        createCanvas = function (){\r\n            console.error(\"createCanvas is disabled\");\r\n        }\r\n        document.getElementById(\"defaultCanvas0\").style.width = \"100vmin\";\r\n        document.getElementById(\"defaultCanvas0\").style.height = \"100vmin\";\r\n        setupArg()\r\n    };\r\n\r\n    for(let func of otherFunctions){\r\n        if(acceptedFunctions.includes(func.name)) {\r\n            window[func.name] = func;\r\n        }\r\n    }\r\n\r\n    window.draw = drawArg;\r\n\r\n    new p5();\r\n}\r\n\r\nfunction __canvasTest(){\r\n    background(0);\r\n    stroke(255);\r\n\r\n    for (let i = 0; i < 500; i+=10) {\r\n        let timeRatio = (frameCount % 500-i) / 490;\r\n        let bRat = i/500;\r\n\r\n        fill(timeRatio * 255, 0, bRat*255);\r\n        rect(timeRatio * 490, 0, 10, 10);\r\n\r\n        fill(0, timeRatio * 255, bRat*255);\r\n        rect(0, timeRatio * 490, 10, 10);\r\n    }\r\n\r\n    fill(255);\r\n\r\n    ellipse(mouseX,mouseY,10,10);\r\n\r\n    textAlign(CENTER);\r\n    text(\"Welcome back\",250,250);\r\n}\r\n\n\n//# sourceURL=webpack:///./src/exec/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/exec/index.js"]();
+/******/ 	
+/******/ })()
+;
