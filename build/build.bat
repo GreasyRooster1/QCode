dcc4c217753e0bc825266079ca7637d8ad325194
GreasyRooster <greasyRooster1@gmail.com>
2025-04-14 11:47:06 -0700

    fix

--- build/build.bat
+++ build/build.bat
@@ -8,4 +8,4 @@ exit /b
 :date
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
-echo %1-%mydate%.%mytime%
+echo dev-%mydate%.%mytime%
