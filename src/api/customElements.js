a7f3338a5803b60028fa0e65086c9e927579c4af
GreasyRooster <greasyRooster1@gmail.com>
2025-05-16 18:26:14 -0700

    remove overlay

--- src/api/customElements.js
+++ src/api/customElements.js
@@ -59,7 +59,7 @@ class StepElement extends HTMLElement {
         this.imageOverlay = document.createElement("img");
         this.imageOverlay.classList.add("step-image-overlay");
         this.imageWrapper.appendChild(this.image)
-        this.imageWrapper.appendChild(this.imageOverlay)
+        //this.imageWrapper.appendChild(this.imageOverlay)
 
         this.textContent = document.createElement("div");
         this.textContent.classList.add("step-text-content");
