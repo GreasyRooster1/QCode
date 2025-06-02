72adcfb3141c2045bff86f05adf8a1e0b598eadc
GreasyRooster <greasyRooster1@gmail.com>
2025-06-02 16:08:05 -0700

    vgjk

--- src/editor/languageTypes/scratch.ts
+++ src/editor/languageTypes/scratch.ts
@@ -2,7 +2,7 @@ import {ProjectType,RunErrCallback} from "./projectType";
 import {getCode,setupEvents as setupExecEvents,logNames,runCode,frameContent,frame,stopFrame} from "../executionHelper"
 import {Language} from "../codeEditor";
 import {getStoredUser} from "../../api/auth";
-import {ref, set} from "firebase/database";
+import {get, ref, set} from "firebase/database";
 import {db} from "../../api/firebase";
 import {writeToEditor} from "../utils/loadUtils";
 import {clearConsole} from "../codeExecution";
@@ -48,15 +48,20 @@ class ScratchType extends ProjectType {
         return "javascript";
     }
 
-    static getProjectDBData(projectName: string, lessonId: string):Object {
-        return {
-            language:"scratch",
-            name: projectName,
-            lessonId:lessonId??"none",
-            currentChapter:0,
-            currentStep:0,
-            timestamp:Date.now()/1000,
-        }
+    static getProjectDBData(projectName: string, lessonId: string):Promise<Object> {
+        let cleanLessonId = lessonId ?? "none"
+        let hasLesson = cleanLessonId != "none";
+        return new Promise((resolve, reject) => {
+            let data = {
+                language: "scratch",
+                name: projectName,
+                lessonId: lessonId ?? "none",
+                currentChapter: 0,
+                currentStep: 0,
+                timestamp: Date.now() / 1000,
+            }
+            resolve(data);
+        });
     }
 }
 
